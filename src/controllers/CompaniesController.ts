a23898652cb73e11c54b853d5d70d92ec17607f8
João Teixeira <jpteixeira1308@gmail.com>
2021-08-11 18:28:28 -0300

    CRUD tabela companies

--- src/controllers/CompaniesController.ts
+++ src/controllers/CompaniesController.ts
@@ -5,15 +5,25 @@ const adminAuth = require('../middlewares/adminAuth')
 
 router.get('/companies', adminAuth.adminAuth, async (req, res) => {
     const result = await knex('companies')
-
-    return res.json(result)
+    if (result.length > 0) {
+        res.status(200)
+        return res.json(result)
+    } else {
+        res.status(500)
+        return res.json({ err: 'Erro itnerno do servidor' })
+    }
 })
 
 router.get('/companie/:id', adminAuth.adminAuth, async (req, res) => {
-    let id = req.params.id
-    const result = await knex('companies').where({ 'id': id }).first()
-
-    return res.json(result)
+    const id = req.params.id
+    const result = await knex('companies').where({ 'id': id })
+    if (result.length > 0) {
+        res.status(200)
+        return res.json(result)
+    } else {
+        res.status(404)
+        return res.json({ err: 'Registro não encontrado' })
+    }
 })
 
 router.post('/companie', adminAuth.adminAuth, async (req, res) => {
@@ -21,20 +31,65 @@ router.post('/companie', adminAuth.adminAuth, async (req, res) => {
     let result = await knex('companies').where({ 'cnpj': cnpj })
     if (result.length > 0) {
         res.status(400)
-        res.send({ err: 'CNPJ já está cadastrado na base de dados!' })
+        res.json({ err: 'CNPJ já está cadastrado na base de dados!' })
     } else {
         await knex('companies').insert({
             razao_social,
             cnpj
         })
         result = await knex('companies').where({ 'cnpj': cnpj })
-        if (result.length > 0) {
+        if (result) {
             res.status(200)
-            res.send({ msg: 'Registro inserido com sucesso!' })
+            return res.json(result)
         } else {
             res.status(500)
-            res.send({ err: 'Falha ao inserir registro' })
+            return res.json({ err: 'Falha ao inserir registro' })
         }
+    }
+})
+
+router.delete('/companie/:id', adminAuth.adminAuth, async (req, res) => {
+    const id = req.params.id
+    let result = await knex('companies').where({ 'id': id })
+    if (result.length > 0) {
+        await knex('companies').delete().where({ 'id': id })
+        result = await knex('companies').where({ 'id': id })
+        if (result.length > 0) {
+            res.status(500)
+            return res.json({ err: 'Falha ao deletar registro' })
+        } else {
+            res.status(200)
+            return res.json({ msg: 'Registro deletado com sucesso!' })
+        }
+    } else {
+        res.status(404)
+        return res.json({ err: 'Registro não encontrado' })
+    }
+})
+
+router.put('/companie/:id', adminAuth.adminAuth, async (req, res) => {
+    const id = req.params.id
+    const { razao_social, cnpj } = req.body
+    let result = await knex('companies').where({ 'id': id })
+    if (result.length > 0) {
+        result = await knex('companies').where({ 'cnpj': cnpj })
+        if (result.length > 0) {
+            res.status(400)
+            res.json({ err: 'CNPJ já está cadastrado na base de dados!' })
+        } else {
+            await knex('companies').where({ 'id': id }).update({ 'razao_social': razao_social, 'cnpj': cnpj })
+            result = await knex('companies').where({ 'cnpj': cnpj })
+            if (result.length > 0) {
+                res.status(200)
+                return res.json(result)
+            } else {
+                res.status(500)
+                return res.json({ err: 'Falha ao alterar registro' })
+            }
+        }
+    } else {
+        res.status(404)
+        return res.json({ err: 'Registro não encontrado' })
     }
 })
 
